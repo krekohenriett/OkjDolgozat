e26f3046053e286ab02bc9932d06099de616d2be
krekohenriett <heni.kreko@gmail.com>
2021-03-29 10:38:08 +0200

    first commit

---
+++ feldolgoz.php
@@ -0,0 +1,23 @@
+<?php
+
+require './MySqlDB.php';
+
+$mySql = new MySqlDB();
+$adat=$_GET['todo'];
+
+$adatok = array();
+$result = $mySql->lekerdez("adatok", "nev like '%".$adat."%'");
+
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+
+        $adatok[] = $row;
+    }
+    echo json_encode($adatok);
+} else {
+    echo "0 results";
+}
+//print_r( $telefonkonyvem);
+
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <link href="stilus.css" rel="stylesheet" type="text/css"/>
+        <script src="feldolgoz.js" type="text/javascript"></script>
+        <title>Krekó Henriett</title>
+    </head>
+    <body>
+                       
+        <header>Teendők</header>
+          <main>
+            <section>
+                <form class="urlap">
+                    <label for="todo">TODO lista:</label><br>
+                    <input type="text" id="todo" name="todo">
+                    <input type="date" id="datum" name="datum">
+                    <input type="button" id="add" name="add" value="Add">
+                    <!--<p id="javaslat"></p>-->
+<!--                    <div id="legordulo">                        
+                    </div>-->
+                </form>
+<!--                <article>
+                    <p id="javaslat"></p>
+                </article>-->
+            </section>
+        </main>
+        
+        <?php
+        
+        ?>
+    </body>
+</html>
